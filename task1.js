ff1202e8c6f90df1b786343048b2f079a07c9a39
Anna <annayattsenko@gmail.com>
2025-04-08 12:04:04 +0300

    tasks

--- task1.js
+++ task1.js
@@ -5,3 +5,16 @@
 // який створений у файлі index.html
 // Запустити програму за допомогою Live Server
 // Перевірити за допомогою команди npm tests/task1.test.js 
+
+const url = "https://jsonplaceholder.typicode.com/users ";
+const list = document.querySelector(".usersList");
+
+fetch(url)
+  .then(response => response.json())
+  .then(users => {
+    users.forEach(user => {
+      const li = document.createElement("li");
+      li.textContent = user.name;
+      list.appendChild(li);
+    });
+  });
--- task2.js
+++ task2.js
@@ -5,3 +5,18 @@
 // відобразити у тезі #userCity
 // Запустити програму потрібно за допомогою Live Server
 // Перевірити правильність програми - команда node tests/task2.test.js
+
+const secondUrl = "https://jsonplaceholder.typicode.com/users";
+const search = document.getElementById("userNameInput");
+const btn = document.getElementById("getUserButton");
+const span = document.getElementById("userCity");
+
+btn.addEventListener("click", () => {
+  fetch(secondUrl)
+    .then(response => response.json())
+    .then(users => {
+      const name = search.value.trim().toLowerCase();
+      const foundUser = users.find(user => user.name.toLowerCase() === name);
+      span.textContent = foundUser.address.city;
+    })
+});
